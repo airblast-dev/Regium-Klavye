43d77d145856cc00710778212915624dfc03ccee
Tayfun Bocek <tayfunbocek@live.ca>
2023-06-15 17:28:51 +0300

    Implement rule writing.

--- udev.py
+++ udev.py
@@ -1,18 +1,17 @@
-import subprocess
 from keyboard_profiles import PROFILES
-from keyboard_profiles.profile_types import Profile, Model
 from typing import List, Tuple
 
 
-def _construct_rule(vid_pid: Tuple[int, int]) -> str:
+UDEV_PATH = "/etc/udev/rules.d/99-rkapi.rules"
 
+
+def _construct_rule(vid_pid: Tuple[int, int]) -> str:
     #  When formatting integer to hexadecimal string leading 0's are removed.
     #  This adds the 0 values while still fitting in 4 characters so its valid.
     #  5e -> 005e
     return (
         f'SUBSYSTEM=="usb", ATTRS{{idVendor}}=="{vid_pid[0]:>04x}", ATTRS{{idProduct}}=="{vid_pid[1]:>04x}", MODE="0666"\n'
-        +
-        f'SUBSYSTEM=="hidraw", ATTRS{{idVendor}}=="{vid_pid[0]:>04x}", ATTRS{{idProduct}}=="{vid_pid[1]:>04x}", MODE="0666"'
+        + f'SUBSYSTEM=="hidraw", ATTRS{{idVendor}}=="{vid_pid[0]:>04x}", ATTRS{{idProduct}}=="{vid_pid[1]:>04x}", MODE="0666"'
     )
 
 
@@ -26,18 +25,47 @@ def _construct_rules() -> Tuple[str, ...]:
 
 
 def _construct_comments() -> List[str]:
-    devices: Tuple[Profile] = tuple(profile for profile in PROFILES.values())
+    devices = tuple(profile for profile in PROFILES.values())
     comments: List[str] = list()
     for device in devices:
         name = device["name"]
         long_name = tuple(model["long_name"] for model in device["models"])
         comments.append(_construct_comment(name, long_name))
     return comments
-        
+
+
 def get_udev() -> str:
-    udev = "#  This file should not be edited manually.\n#  Use command xx.\n\n" #  TO-DO add command name for cli implementation.
+    """
+    Generate udev rules.
+
+    Example result:
+    #  This file should not be edited manually.
+    #  Use command
+
+    #  Rules for device Royal Kludge RK68. ('Royal Kludge RK68 BT and USB',)
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="258a", ATTRS{idProduct}=="005e", MODE="0666"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="258a", ATTRS{idProduct}=="005e", MODE="0666"
+    """
+    udev = "#  This file should not be edited manually.\n#  Use command xx.\n\n"  #  TO-DO add command name for cli implementation.
     for comment, rule in zip(_construct_comments(), _construct_rules()):
-        udev += (comment + rule + "\n\n")
+        udev += comment + rule + "\n\n"
     return udev
 
-print(get_udev())
+
+def write_udev(path: str = UDEV_PATH, refresh_rules: bool = True) -> None:
+    """Write the rules to udev path, requires administrator priveleges."""
+    with open(path, "w") as file:
+        file.write(get_udev())
+
+
+def setup_rules() -> None:
+    """Write rules to udev path, reload rules and trigger them."""
+    write_udev()
+
+    import subprocess
+
+    #  In case rules fail to reload automatically.
+    subprocess.run(["udevadm", "control", "--reload"])
+
+    #  Force udev to trigger rules.
+    subprocess.run(["udevadm", "trigger"])
