b237dd83810b0da778953049bd31c65766199923
Tayfun Bocek <tayfunbocek@live.ca>
2023-06-15 17:27:48 +0300

    Code formatting

--- tests/test_key.py
+++ tests/test_key.py
@@ -4,16 +4,15 @@ from keyboard_parts import Key
 
 
 class KeyColorTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.key = Key("A")
+        self.key = Key("A", ((0, 2), (0, 3), (0, 4)))
 
     def test_key_set_color(self):
-        test_values = [[5, 10, 15], [100, 200, 150], [100, 200, 255],
-                       [10, 20, 30]]
+        test_values = [[5, 10, 15], [100, 200, 150], [100, 200, 255], [10, 20, 30]]
         for value in test_values:
             with self.subTest(
-                    msg="Set colors using Sequence of integers.", value=value):
+                msg="Set colors using Sequence of integers.", value=value
+            ):
                 self.key.set_color(value)
                 self.assertEqual(self.key.get_color(), value)
 
@@ -21,8 +20,8 @@ class KeyColorTestCase(unittest.TestCase):
         test_values = ["WumboSized", ("A", 2, 3)]
         for value in test_values:
             with self.subTest(
-                    msg="Set invalid colors using color keywords. (red, green, blue)",
-                    value=value,
+                msg="Set invalid colors using color keywords. (red, green, blue)",
+                value=value,
             ):
                 self.assertRaises(
                     TypeError,
@@ -32,30 +31,27 @@ class KeyColorTestCase(unittest.TestCase):
                     blue=value[2],
                 )
             with self.subTest(
-                    msg="Set invalid colors using Sequence of integers.",
-                    value=value):
+                msg="Set invalid colors using Sequence of integers.", value=value
+            ):
                 self.assertRaises(TypeError, self.key.set_color, value)
 
 
 class KeyIndexTestCase(unittest.TestCase):
-
     def test_key_index(self):
         with self.subTest(msg="Test valid index values."):
             key = Key("A", ((5, 6), (7, 8), (9, 0)))
             self.assertEqual(key.indexes, ((5, 6), (7, 8), (9, 0)))
 
     def test_key_index_invalid_arg(self):
-        test_values_type = [
-            "Hello", ("s", (1, 2), (2, 3)), ((5, "A"), (1, 2), (2, 3))
-        ]
+        test_values_type = ["Hello", ("s", (1, 2), (2, 3)), ((5, "A"), (1, 2), (2, 3))]
         test_values_value = [((1, 2), (3, 4)), ((5, 6), (7, 8), (9, 0, 3)), []]
         for value in test_values_type:
             with self.subTest(
-                    msg="Test invalid index values for TypeError.",
-                    value=value):
+                msg="Test invalid index values for TypeError.", value=value
+            ):
                 self.assertRaises(TypeError, Key, "A", value)
         for value in test_values_value:
             with self.subTest(
-                    msg="Test invalid index values for ValueError.",
-                    value=value):
+                msg="Test invalid index values for ValueError.", value=value
+            ):
                 self.assertRaises(ValueError, Key, "A", value)
--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -6,20 +6,18 @@ from keyboard_profiles import PROFILES
 
 
 class TestKeyboard(Keyboard):
-
     def __init__(self, vid: int, pid: int):
         super().__init__(vid, pid)
 
     def apply_color(
-            self,
-            rgb: Optional[Tuple[int, int, int]] = None) -> tuple[bytearray]:
+        self, rgb: Optional[Tuple[int, int, int]] = None
+    ) -> tuple[bytearray]:
         self._color_data()
         self.set_color((0, 0, 0))
         return self._final_color_data
 
 
 class KeyboardsTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
         keyboards = []
         for device in PROFILES.keys():
@@ -39,16 +37,16 @@ class KeyboardsTestCase(unittest.TestCase):
             keyboard.set_color(colors)
             for key in keyboard:
                 with self.subTest(
-                        msg="Test keyboard settings through each key.",
-                        key=key):
+                    msg="Test keyboard settings through each key.", key=key
+                ):
                     self.assertEqual(key.get_color(), colors)
 
     def test_set_color_invalid_arg(self):
         test_values = ["WumboSized", ("A", 2, 3)]
         for keyboard, value in zip(self.keyboards, test_values):
             with self.subTest(
-                    msg="Set invalid colors using color keywords. (red, green, blue)",
-                    value=value,
+                msg="Set invalid colors using color keywords. (red, green, blue)",
+                value=value,
             ):
                 self.assertRaises(
                     TypeError,
@@ -56,12 +54,12 @@ class KeyboardsTestCase(unittest.TestCase):
                     rgb=value,
                 )
             with self.subTest(
-                    msg="Set invalid colors using Sequence of integers.",
-                    value=value):
+                msg="Set invalid colors using Sequence of integers.", value=value
+            ):
                 self.assertRaises(TypeError, keyboard.set_color, value)
             with self.subTest(
-                    msg="Set invalid colors using 3 color values as seperate arguments.",
-                    value=value,
+                msg="Set invalid colors using 3 color values as seperate arguments.",
+                value=value,
             ):
                 self.assertRaises(TypeError, keyboard.set_color, *value)
 
@@ -79,8 +77,8 @@ class KeyboardsTestCase(unittest.TestCase):
         label = "A"
         for keyboard, value in zip(self.keyboards, test_values):
             with self.subTest(
-                    msg="Set invalid colors using color keywords. (red, green, blue)",
-                    value=value,
+                msg="Set invalid colors using color keywords. (red, green, blue)",
+                value=value,
             ):
                 self.assertRaises(
                     TypeError,
@@ -92,12 +90,10 @@ class KeyboardsTestCase(unittest.TestCase):
     def test_set_key_color_data(self):
         for keyboard in self.keyboards:
             profile = PROFILES[(keyboard._vid, keyboard._pid)]
-            present_keys: Tuple[str,
-                                Tuple[Tuple[int, int], Tuple[int, int],
-                                      Tuple[int,
-                                            int]]] = profile["present_keys"]
-            colors = ((0xFF, 0x10, 0x20), (0x10, 0xFF, 0x20), (0x10, 0x20,
-                                                               0xFF))
+            present_keys: Tuple[
+                str, Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int]]
+            ] = profile["present_keys"]
+            colors = ((0xFF, 0x10, 0x20), (0x10, 0xFF, 0x20), (0x10, 0x20, 0xFF))
             for key in present_keys:
                 indexes = key[1]
                 for color in colors:
@@ -106,11 +102,12 @@ class KeyboardsTestCase(unittest.TestCase):
 
                     for i in range(0, 3):
                         with self.subTest(
-                                msg="Compare bytearray results to color value.",
-                                label=key[0],
-                                step=(indexes[i][0], indexes[i][1]),
-                                data=data[indexes[i][0]],
-                                value=color[i],
+                            msg="Compare bytearray results to color value.",
+                            label=key[0],
+                            step=(indexes[i][0], indexes[i][1]),
+                            data=data[indexes[i][0]],
+                            value=color[i],
                         ):
-                            self.assertEqual(data[indexes[i][0]][indexes[i][1]],
-                                             color[i])
+                            self.assertEqual(
+                                data[indexes[i][0]][indexes[i][1]], color[i]
+                            )
