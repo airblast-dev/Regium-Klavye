a916e9dcf470d445da1674c8a3357946f14283f9
Tayfun Bocek <tayfunbocek@live.ca>
2023-06-23 23:20:25 +0300

    Update and organize Imports

--- keyboard_profiles/profile_types/__init__.py
+++ keyboard_profiles/profile_types/__init__.py
@@ -1,3 +1,11 @@
 from .profile_type import Profile
 from .model import Model
-from .commands import Commands, Colors, AnimationOption, AnimationParam, Animations
+from .commands import (
+    Commands,
+    Colors,
+    ColorParams,
+    ColorParam,
+    AnimationOption,
+    AnimationParam,
+    Animations,
+)
