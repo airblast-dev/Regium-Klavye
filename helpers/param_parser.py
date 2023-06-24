b3458bef9ccc9a2262efba4a3aa869dada4c636a
Tayfun Bocek <tayfunbocek@live.ca>
2023-06-24 04:32:22 +0300

    Variable renaming options -> params

--- helpers/param_parser.py
+++ helpers/param_parser.py
@@ -2,50 +2,49 @@ from keyboard_profiles.profile_types import AnimationParam, ColorParam
 
 
 def parse_params(
-    options: dict[str, int] | None,
-    kb_options: dict[str, AnimationParam | ColorParam],
+    params: dict[str, int] | None,
+    kb_params: dict[str, AnimationParam | ColorParam],
 ) -> dict[str, list[int]]:
     """Parse parameters and fill in the missing parameters in options with defaults defined in kb_option."""
 
-    if options is None:
-        default_options = {}
-        for param in kb_options.keys():
-            default_options[param] = kb_options[param]["default"]
-        return default_options
+    if params is None:
+        default_params = {}
+        for param in kb_params.keys():
+            default_params[param] = kb_params[param]["default"]
+        return default_params
 
-    if (options_s := set(options.keys())) != (color_param_s := set(kb_options)):
-        extra_param = options_s - color_param_s
+    if (param_s := set(params.keys())) != (color_param_s := set(kb_params)):
+        extra_param = param_s - color_param_s
         ValueError(
-            f"Expected one or more from ({kb_options.keys()}. Found {sorted(extra_param)})"
+            f"Expected one or more from ({kb_params.keys()}. Found {sorted(extra_param)})"
         )
 
-    _options = {}
-    for k, v in options.items():
+    _params = {}
+    for k, v in params.items():
         if type(v) is int:
-            _options[k] = [v]
+            _params[k] = [v]
         elif isinstance(v, (list, tuple)):
-            _options[k] = v
+            _params[k] = v
 
-    new_options = {}
-    for param in kb_options.keys():
-        print(kb_options.keys())
+    new_params = {}
+    for param in kb_params.keys():
+        print(kb_params.keys())
         is_valid: bool = False
-        if param not in _options.keys():
-            print(kb_options[param])
-            new_options[param] = kb_options[param]["default"]
+        if param not in _params.keys():
+            new_params[param] = kb_params[param]["default"]
             continue
 
         # Check if provided value is in accepted parameter.
-        if callable(kb_options[param]["checks"]):
+        if callable(kb_params[param]["checks"]):
             #  Below is type ignored as type checkers include the range functions return as a callable.
-            is_valid = kb_options[param]["checks"](_options[param])  # type: ignore
-        elif isinstance(kb_options[param]["checks"], (list, tuple, range)):
+            is_valid = kb_params[param]["checks"](_params[param])  # type: ignore
+        elif isinstance(kb_params[param]["checks"], (list, tuple, range)):
             is_valid = all(
-                [value in kb_options[param]["checks"] for value in _options[param]]
+                [value in kb_params[param]["checks"] for value in _params[param]]
             )
         if is_valid is True:
-            new_options[param] = _options[param]
+            new_params[param] = _params[param]
             continue
         raise ValueError(f"Invalid value provided for {param}.")
 
-    return new_options
+    return new_params
