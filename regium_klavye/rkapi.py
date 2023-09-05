fa3a93a0bbd16728bbaa8518d98b3f30aace18f8
Tayfun Bocek <tayfunbocek@live.ca>
2023-09-05 13:50:33 +0300

    Group device enumeration.

--- regium_klavye/rkapi.py
+++ regium_klavye/rkapi.py
@@ -5,27 +5,34 @@ from .keyboard_parts import Keyboard
 from .keyboard_profiles import PROFILES
 
 
+def _enumerate_devices(vid: int | None = None, pid: int | None = None) -> list[dict]:
+    match vid, pid:
+        case None, None:
+            return hid.enumerate()
+        case int(vid), None:
+            return hid.enumerate(vid)
+        case int(vid), int(pid):
+            return hid.enumerate(vid, pid)
+        case _, int(pid):
+            raise ValueError("Cannot take product id without vendor id.")
+        case _, _:
+            raise TypeError(f"Expected int or None, found {type(vid)} and {type(pid)}")
+
+
 def get_keyboards(vid: int | None = None, pid: int | None = None) -> list[Keyboard]:
     """Get all connected and supported keyboards.
 
     Providing a vendor ID will only return keyboards with matching information.
     Providing a product ID will only return keyboards with matching information.
 
-    Parameters
-    __________
-    vid: Optional[:class:`int`]
-        Vendor ID of keyboards to get.
-    pid: Optional[:class:`int`]
-        Product ID of keyboards to get.
+    Args:
+        vid: Optional[:class:`int`]
+            Vendor ID of keyboards to get.
+        pid: Optional[:class:`int`]
+            Product ID of keyboards to get.
     """
     keyboards = []
-    if vid is None and pid is None:
-        enumerated_devices = hid.enumerate()
-    elif vid is not None and pid is None:
-        enumerated_devices = hid.enumerate(vid)
-    else:
-        enumerated_devices = hid.enumerate(vid, pid)
-    for device in enumerated_devices:
+    for device in _enumerate_devices(vid, pid):
         dev_profile = PROFILES.get((device["vendor_id"], device["product_id"]))
         if dev_profile is None:
             continue
@@ -36,9 +43,10 @@ def get_keyboards(vid: int | None = None, pid: int | None = None) -> list[Keyboa
     return keyboards
 
 
-def get_keyboard(vid: int, pid: int) -> Keyboard:
+def get_keyboard(vid: int, pid: int | None = None) -> Keyboard:
     """Get a single Keyboard object found with the provided vid and pid.
 
+    If only a vendor ID is provided, returns first supported device matching vendor ID.
     If a supported device is not found NoKeyboardsFound exception is Raised.
 
     Args:
@@ -48,7 +56,7 @@ def get_keyboard(vid: int, pid: int) -> Keyboard:
     Raises:
         KeyboardNotFoundError: Requested keyboard was not found.
     """
-    for device in hid.enumerate(vid, pid):
+    for device in _enumerate_devices(vid, pid):
         dev_profile = PROFILES.get((device["vendor_id"], device["product_id"]))
         if dev_profile is None:
             continue
@@ -61,7 +69,7 @@ def get_keyboard(vid: int, pid: int) -> Keyboard:
 class KeyboardNotFoundError(Exception):
     """Raised if a single keyboard was requested but it wasnt found."""
 
-    def __init__(self, vid: int, pid: int):
+    def __init__(self, vid: int, pid: int | None):
         super().__init__(
             f"Unable to find interface with the vendor id of "
             f"{vid} and product id of {pid}."
